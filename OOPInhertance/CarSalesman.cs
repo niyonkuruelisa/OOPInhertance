64df284516f58cddd402ebe847dc65cecf0fb8ed
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2022-06-25 10:39:41 +0200

    Add project files.

---
+++ OOPInhertance/CarSalesman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInhertance
+{
+    internal class CarSalesman
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public CarSalesman(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}",_firstName,_lastName);
+            }
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine(string.Format("Hello! My name is {0}, I want you to buy this car.",FullName));
+        }
+    }
+}
---
+++ OOPInhertance/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPInhertance;
+
+
+App();
+
+void App()
+{
+    //  Create an instance from a class
+    CarSalesman carSalesman1 = new CarSalesman("Niyonkuru", "Elisa");
+    Console.WriteLine(carSalesman1.FullName);
+    carSalesman1.Sell();
+}
